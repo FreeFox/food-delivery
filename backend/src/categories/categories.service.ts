a0cf5e16ea0ab8ead7019ac2f2d89a86fdab2dfb
Roman Lysiuk <roma.freefox@gmail.com>
2026-02-19 18:33:50 +0200

    Prisma - categories API

--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -2,6 +2,7 @@ import { HttpStatus, Injectable, NotFoundException, HttpException } from '@nestj
 import type { CreateCategoryDto } from './dto/create-category.dto';
 import { Prisma, Category } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoriesService {
@@ -43,6 +44,47 @@ export class CategoriesService {
       });
     } catch (error) {
       throw new HttpException('Failed to create category', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async update(where: Prisma.CategoryWhereUniqueInput, data: UpdateCategoryDto): Promise<Category> {
+    try {
+      return await this.prisma.category.update({
+          where,
+          data: {
+            ...data
+          }
+      });
+    } catch (error) {
+      throw new HttpException('Failed to update category', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async replace(where: Prisma.CategoryWhereUniqueInput, data: CreateCategoryDto): Promise<Category> {
+    try {
+      return await this.prisma.category.upsert({
+          where,
+          update: {
+            ...data,
+            icon: data.icon || '', // Set to empty string if not provided
+          },
+          create: {
+            ...data,
+            icon: data.icon || '', // Set to empty string if not provided
+          }
+      });
+    } catch (error) {
+      throw new HttpException('Failed to replace category', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async delete(where: Prisma.CategoryWhereUniqueInput): Promise<Category> {
+    try {
+      return await this.prisma.category.delete({
+          where
+      });
+    } catch (error) {
+      throw new HttpException('Failed to delete category', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
